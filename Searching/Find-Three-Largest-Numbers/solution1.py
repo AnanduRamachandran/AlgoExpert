dbc3fae313e3d5251ed2ac7bb45a6aee5f9bd836
AnanduRamachandran <ananduyou@gmail.com>
2021-07-03 12:50:14 +0530

    Three largest numbers solution 1 added

---
+++ Searching/Find-Three-Largest-Numbers/solution1.py
@@ -0,0 +1,16 @@
+def findThreeLargestNumbers(array):
+	a = b = c = float("-inf")
+	for i in range(len(array)):
+		if array[i] > a:
+			if array[i] > b:
+				if array[i] > c:
+					a = b
+					b = c
+					c = array[i]
+				else:
+					a = b
+					b = array[i]
+			else:
+				a = array[i]
+	return [a,b,c]
+				
